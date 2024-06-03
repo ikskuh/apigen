466e0bc4946611b4a517716104a766babe0bbe39
Felix "xq" Queißner <git@random-projects.net>
2024-06-03 20:15:12 +0200

    Adds annotation

--- src/debug-support.zig
+++ src/debug-support.zig
@@ -2,6 +2,10 @@
 //! This file is not necessary to use apigen, but is compiled in when building with Zig
 //! so additional crash diagnostic capabilities are unlocked.
 //!
+//!
+//! "Inspired" by:
+//! https://gist.github.com/andrewrk/752a1f4db1a3327ee21f684cc87026e7
+//!
 
 const std = @import("std");
 
